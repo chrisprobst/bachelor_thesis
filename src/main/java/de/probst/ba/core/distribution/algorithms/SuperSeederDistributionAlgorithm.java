1e0de7522628136652ff18dc664029e1cadda1f0
Christopher Probst <christopher.probst@hhu.de>
2014-09-16 22:19:57 +0200

    Format

--- src/main/java/de/probst/ba/core/distribution/algorithms/SuperSeederDistributionAlgorithm.java
+++ src/main/java/de/probst/ba/core/distribution/algorithms/SuperSeederDistributionAlgorithm.java
@@ -2,9 +2,9 @@ package de.probst.ba.core.distribution.algorithms;
 
 import de.probst.ba.core.distribution.SeederDistributionAlgorithm;
 import de.probst.ba.core.media.database.DataInfo;
-import de.probst.ba.core.net.peer.transfer.TransferManager;
 import de.probst.ba.core.net.peer.PeerId;
 import de.probst.ba.core.net.peer.Seeder;
+import de.probst.ba.core.net.peer.transfer.TransferManager;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -24,33 +24,22 @@ public final class SuperSeederDistributionAlgorithm implements SeederDistributio
             return dataInfo;
         }
 
-        if (!totallyEmpty) {
-            Map<String, DataInfo> copy = new HashMap<>(dataInfo);
-            alreadyUploaded.forEach(x -> {
-                DataInfo extracted = copy.get(x.getHash()).subtract(x);
-                if (extracted.isEmpty()) {
-                    copy.remove(extracted.getHash());
-                } else {
-                    copy.put(extracted.getHash(), extracted);
-                }
-            });
-            if (copy.isEmpty()) {
-                totallyEmpty = true;
+        Map<String, DataInfo> copy = new HashMap<>(dataInfo);
+        alreadyUploaded.forEach(x -> {
+            DataInfo extracted = copy.get(x.getHash()).subtract(x);
+            if (extracted.isEmpty()) {
+                copy.remove(extracted.getHash());
+            } else {
+                copy.put(extracted.getHash(), extracted);
             }
-            return copy;
-        } else {
-            return dataInfo;
-        }
+        });
+        return copy;
     }
 
-    private boolean totallyEmpty = false;
     private final Set<DataInfo> alreadyUploaded = new HashSet<>();
 
     @Override
     public synchronized boolean isUploadAllowed(Seeder seeder, TransferManager transferManager) {
-        if (totallyEmpty) {
-            return true;
-        }
         return alreadyUploaded.add(transferManager.getTransfer().getDataInfo());
     }
 }
