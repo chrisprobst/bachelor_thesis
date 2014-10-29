f2cd374c044244e27c69c6e5aedf547e2be6a4d3
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2014-10-29 11:09:07 +0100

    Update scripts

--- tools/python/scenarios/plot_specific.sh
+++ tools/python/scenarios/plot_specific.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+echo $1
+echo $2
+echo $3
+
 BASEDIR="../python/scenarios/results/"
 SCENARIO=$1
 DIR=$2
+SCRIPT=$3
 BASEDIR=$BASEDIR$SCENARIO'/'$DIR'/'
 
 cd ../../gnuplot
 pwd
 echo $BASEDIR
-gnuplot -e "dir='$BASEDIR'" plot.cfg
+gnuplot -e "dir='$BASEDIR'" "plot_specific_$SCRIPT.cfg"
 
 open img
