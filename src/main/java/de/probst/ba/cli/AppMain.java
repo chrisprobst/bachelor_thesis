696100c7d2307dd04d517d2747c2009b0aa8a657
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2014-11-05 19:08:37 +0100

    Add results to repo

---
+++ src/main/java/de/probst/ba/cli/AppMain.java
@@ -0,0 +1,14 @@
+package de.probst.ba.cli;
+
+/**
+ * Created by chrisprobst on 04.11.14.
+ */
+public class AppMain {
+
+
+    public static void main(String[] args) {
+        double i = (int) 1.0 / 100 * 100;
+        System.out.println(i);
+
+    }
+}
